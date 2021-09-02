53e4b0ae3847dc7fb4ce15f81128d26c320eaba5
anderzx <88548362+anderzx@users.noreply.github.com>
2021-09-02 18:36:16 -0500

    Add files via upload

---
+++ sumadosnumeros.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+printf "\n\e[1;92mCreado por Anderzth\n"
+figlet Anderzth
+
+read -p 'Ingresa numero 1= ' a
+read -p 'Ingreso numero 2= ' b
+
+let suma=$a+$b
+echo "La suma de $a + $b es= $suma"
+
+printf "\n\e[1;92mGrupo Telegram: https://t.me/TheRealHacking "
+exit 0
+
